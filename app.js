81b0e41c52d4cef01edf3bc7687c82ef34a809fd
christianarredondo <christianarredondo@utexas.edu>
2017-08-20 17:32:15 -0500

    begin Section 7 - 'Doing Contract Work with Interfaces'

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('System initialized');
+function greeter(person) {
+    console.log('Hello ' + person.firstName);
+}
+function changeName(person) {
+    person.firstName = 'Someone Else';
+}
+var human = {
+    firstName: 'Christian',
+    age: 27,
+    hobbies: ['Cooking', 'Sports', 'Swangin']
+};
+console.log(human);
+greeter(human);
+changeName(human);
+greeter(human);
--- app.ts
+++ app.ts
@@ -1,1 +1,26 @@
-console.log('System initialized');
+// interfaces and properties
+interface NamedHuman {
+  firstName: string;
+  age?: number;
+  [propName: string]: any;
+}
+
+function greeter (person: NamedHuman) {
+  console.log('Hello ' + person.firstName);
+} 
+
+function changeName (person: NamedHuman) {
+  person.firstName = 'Someone Else';
+}
+
+const human = {
+  firstName: 'Christian',
+  age: 27,
+  hobbies: ['Cooking', 'Sports', 'Swangin']
+}
+
+console.log(human);
+greeter(human);
+changeName(human);
+greeter(human);
+
