20b67451aa365a2126f12174a983b222295d3c59
christianarredondo <christianarredondo@utexas.edu>
2017-08-21 23:53:07 -0500

    completea activities for arrays, generics types, and generic classes

--- app.js
+++ app.js
@@ -10,6 +10,31 @@ console.log(echo({ name: 'Christian', age: 26 }));
 function betterEcho(data) {
     return data;
 }
-console.log(betterEcho('Christian').length);
+console.log(betterEcho('Christian'));
 console.log(betterEcho(26));
 console.log(betterEcho({ name: 'Christian', age: 26 }));
+// built-in generics
+var testArray = [1.01, 4.93];
+testArray.push(-84);
+console.log(testArray);
+// arrays
+function printAll(args) {
+    args.forEach(function (element) { return console.log(element); });
+}
+printAll(['apples, kiwis, lemons']);
+// generic types
+var echo2 = betterEcho;
+console.log(echo2('Something'));
+// generic class
+var SimpleMath = (function () {
+    function SimpleMath() {
+    }
+    SimpleMath.prototype.calculate = function () {
+        return +this.baseVal * +this.multiplyVal;
+    };
+    return SimpleMath;
+}());
+var simpleMath = new SimpleMath();
+simpleMath.baseVal = '5';
+simpleMath.multiplyVal = 7;
+console.log(simpleMath.calculate());
--- app.ts
+++ app.ts
@@ -10,6 +10,35 @@ console.log(echo({name: 'Christian', age: 26}));
 function betterEcho <T>(data: T) {
   return data;
 }
-console.log(betterEcho('Christian').length);
+console.log(betterEcho('Christian'));
 console.log(betterEcho(26));
 console.log(betterEcho({name: 'Christian', age: 26}));
+
+// built-in generics
+const testArray: Array<number> = [1.01, 4.93];
+testArray.push(-84);
+console.log(testArray);
+
+// arrays
+function printAll<T> (args: T[]) {
+  args.forEach(element => console.log(element));
+}
+printAll<string>(['apples, kiwis, lemons']);
+
+// generic types
+const echo2: <T> (data: T) => T = betterEcho;
+console.log(echo2<string>('Something'));
+
+// generic class
+class SimpleMath<T extends number | string>{
+  baseVal: T;
+  multiplyVal: T;
+  calculate (): number {
+    return +this.baseVal * +this.multiplyVal;
+  }
+}
+
+const simpleMath = new SimpleMath();
+simpleMath.baseVal = '5';
+simpleMath.multiplyVal = 7;
+console.log(simpleMath.calculate());
