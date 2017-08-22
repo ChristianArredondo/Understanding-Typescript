57e4f327ec5208a1c6b86c20dfa7004971fdf484
christianarredondo <christianarredondo@utexas.edu>
2017-08-21 23:19:23 -0500

    set up and complete intro to Section 8 - 'Generics'

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 "use strict";
-function greeter(person) {
-    console.log('Hello ' + person.firstName);
+// simple generic
+function echo(data) {
+    return data;
 }
-function changeName(person) {
-    person.firstName = 'Someone Else';
+console.log(echo('Christian').length);
+console.log(echo(26));
+console.log(echo({ name: 'Christian', age: 26 }));
+// better generic
+function betterEcho(data) {
+    return data;
 }
-var human = {
-    firstName: 'Christian',
-    age: 27,
-    hobbies: ['Cooking', 'Sports', 'Swangin'],
-    greet: function (lastName) {
-        console.log('My last name is', lastName);
-    }
-};
-console.log(human);
-greeter(human);
-changeName(human);
-greeter(human);
-human.greet('What');
-var Humanoid = (function () {
-    function Humanoid() {
-    }
-    Humanoid.prototype.greet = function (lastName) {
-        console.log(lastName);
-    };
-    return Humanoid;
-}());
-var myPerson = new Humanoid();
-myPerson.firstName = 'Christian Ivan';
-greeter(myPerson);
-myPerson.greet('Anything');
-var myDoubleFunction;
-myDoubleFunction = function (v1, v2) {
-    return (v1 + v2) * 2;
-};
-console.log(myDoubleFunction(8, 10));
-var elderPerson = {
-    age: 27,
-    firstName: 'Chris',
-    greet: function (lastName) {
-        console.log(this.firstName + ' ' + lastName);
-    }
-};
-console.log(elderPerson);
-elderPerson.greet('Last Name');
+console.log(betterEcho('Christian').length);
+console.log(betterEcho(26));
+console.log(betterEcho({ name: 'Christian', age: 26 }));
--- app.ts
+++ app.ts
@@ -1,68 +1,15 @@
-// interfaces and classes
-interface NamedHuman {
-  firstName: string;
-  age?: number;
-  [propName: string]: any;
-  greet (lastName: string): void;
+// simple generic
+function echo (data: any) {
+  return data;
 }
+console.log(echo('Christian').length);
+console.log(echo(26));
+console.log(echo({name: 'Christian', age: 26}));
 
-function greeter (person: NamedHuman) {
-  console.log('Hello ' + person.firstName);
-} 
-
-function changeName (person: NamedHuman) {
-  person.firstName = 'Someone Else';
+// better generic
+function betterEcho <T>(data: T) {
+  return data;
 }
-
-const human: NamedHuman = {
-  firstName: 'Christian',
-  age: 27,
-  hobbies: ['Cooking', 'Sports', 'Swangin'],
-  greet (lastName: string) {
-    console.log('My last name is', lastName)
-  }
-}
-
-console.log(human);
-greeter(human);
-changeName(human);
-greeter(human);
-human.greet('What');
-
-class Humanoid implements NamedHuman {
-  firstName: string;
-  greet (lastName: string) {
-    console.log(lastName);
-  }
-}
-
-const myPerson = new Humanoid();
-myPerson.firstName = 'Christian Ivan';
-greeter(myPerson);
-myPerson.greet('Anything');
-
-// Function Types
-interface DoubleValueFunc {
-  (number1: number, number2: number): number;
-}
-
-let myDoubleFunction: DoubleValueFunc;
-myDoubleFunction = function(v1: number, v2: number) {
-  return (v1 + v2) * 2;
-}
-console.log(myDoubleFunction(8,10));
-
-// interface inheritance
-interface AgedPerson extends NamedHuman {
-  age: number;
-}
-
-const elderPerson : AgedPerson = {
-  age: 27,
-  firstName: 'Chris',
-  greet(lastName: string) {
-    console.log(this.firstName + ' ' + lastName);
-  }
-}
-console.log(elderPerson);
-elderPerson.greet('Last Name');
+console.log(betterEcho('Christian').length);
+console.log(betterEcho(26));
+console.log(betterEcho({name: 'Christian', age: 26}));
