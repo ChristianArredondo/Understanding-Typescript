55c6dec42c949b29c4293f640e42161baf3542f1
christianarredondo <christianarredondo@utexas.edu>
2017-08-21 20:11:55 -0500

    complete activities - 'interfaces and methods'

--- app.js
+++ app.js
@@ -8,9 +8,25 @@ function changeName(person) {
 var human = {
     firstName: 'Christian',
     age: 27,
-    hobbies: ['Cooking', 'Sports', 'Swangin']
+    hobbies: ['Cooking', 'Sports', 'Swangin'],
+    greet: function (lastName) {
+        console.log('My last name is', lastName);
+    }
 };
 console.log(human);
 greeter(human);
 changeName(human);
 greeter(human);
+human.greet('What');
+var Humanoid = (function () {
+    function Humanoid() {
+    }
+    Humanoid.prototype.greet = function (lastName) {
+        console.log(lastName);
+    };
+    return Humanoid;
+}());
+var myPerson = new Humanoid();
+myPerson.firstName = 'Christian Ivan';
+greeter(myPerson);
+myPerson.greet('Anything');
--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-// interfaces and properties
 interface NamedHuman {
   firstName: string;
   age?: number;
   [propName: string]: any;
+  greet (lastName: string): void;
 }
 
 function greeter (person: NamedHuman) {
@@ -16,11 +16,26 @@ function changeName (person: NamedHuman) {
 const human = {
   firstName: 'Christian',
   age: 27,
-  hobbies: ['Cooking', 'Sports', 'Swangin']
+  hobbies: ['Cooking', 'Sports', 'Swangin'],
+  greet (lastName: string) {
+    console.log('My last name is', lastName)
+  }
 }
 
 console.log(human);
 greeter(human);
 changeName(human);
 greeter(human);
+human.greet('What');
 
+class Humanoid implements NamedHuman {
+  firstName: string;
+  greet (lastName: string) {
+    console.log(lastName);
+  }
+}
+
+const myPerson = new Humanoid();
+myPerson.firstName = 'Christian Ivan';
+greeter(myPerson);
+myPerson.greet('Anything');
